c685b83fac62589753397a400e53dc6eb5d1b9ed
Valentin Dimov <balentin.dimov@gmail.com>
2025-07-18 18:44:09 +0300

    feat: add comments for the TODOs in the interface
    Signed-off-by: Valentin Dimov <balentin.dimov@gmail.com>

--- tinyjail.h
+++ tinyjail.h
@@ -1,16 +1,22 @@
 #pragma once
 
 struct tinyjailContainerParams {
-    char* containerDir; // System path
-    char** commandList; // NULL-terminated argv
-    char** environment; // NULL-terminated envp
+    // Run parameters of the container process
+    char* containerDir; // Path to root directory of container
+    char** commandList; // NULL-terminated argv for the container init process
+    char** environment; // NULL-terminated envp for the container init process
+    // TODO working directory of the container init process
+    // TODO list of directories to overlay the root directory on top of (workdir in /tmp/...)
+    // sudo mount -t overlay overlay -olowerdir=./lower:./lower2,upperdir=./upper,workdir=./work ./upper
 
+    // Cgroup resource limits for the container
     unsigned long cpuMaxPercent; // Integer 1-100, or 0 if no limit
     unsigned long cpuWeight; // Integer 100-10000, or 0 if no limit
     unsigned long memoryHigh; // Integer > 0 (bytes), or 0 if no limit
     unsigned long memoryMax; // Integer > 0 (bytes), or 0 if no limit
     unsigned long pidsMax; // Integer > 0 (number of pids), or 0 if no limit
 
+    // Networking settings for the container
     char* networkBridgeName; // String or NULL if no network is needed for the container. Mandatory if networkIpAddr or networkDefaultRoute is specified.
     char* networkIpAddr; // String or NULL if no IP address is to be used for the container.
     char* networkDefaultRoute; // String or NULL if no default route is to be used for the container.

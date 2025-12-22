a730a96747bae9fc887e748d4f6c3951a9efe585
Valentin Dimov <balentin.dimov@gmail.com>
2025-12-22 21:03:28 +0100

    refactor: fix directory and error behavior of build.sh
    Signed-off-by: Valentin Dimov <balentin.dimov@gmail.com>

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
 mkdir -p build
 musl-gcc -O2 -Wall -pedantic-errors -s -static -fvisibility=hidden -fPIC -shared src/lib/*.c -o build/libtinyjail.so
 musl-gcc -O2 -Wall -pedantic-errors -s -static src/lib/*.c src/main.c -o build/tinyjail
